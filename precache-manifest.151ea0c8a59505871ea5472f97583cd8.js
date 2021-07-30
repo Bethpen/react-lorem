f0d51b8401df4886c54365c03ee0143081a57335
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-30 22:22:19 +0100

    Updates

---
+++ precache-manifest.151ea0c8a59505871ea5472f97583cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f5e00bba04b6b70c31ca7b9887ada5",
+    "url": "/react-lorem/index.html"
+  },
+  {
+    "revision": "8fde4b9ad921f45e695b",
+    "url": "/react-lorem/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/react-lorem/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lorem/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fde4b9ad921f45e695b",
+    "url": "/react-lorem/static/js/main.f8bbdd36.chunk.js"
+  },
+  {
+    "revision": "394b75bdbdcf8467fd28",
+    "url": "/react-lorem/static/js/runtime-main.51d0ad81.js"
+  }
+]);
